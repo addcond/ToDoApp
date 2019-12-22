4c9f6c0a933de2f6cbc16d4a1378e009d184dec8
addcond <addcond220@gmail.com>
2019-12-23 00:32:13 +0200

    Рабочая тема

--- src/router/link.js
+++ src/router/link.js
@@ -1,4 +1,5 @@
 import {routerObserver} from "./RouterObserver.js";
+
 export default function link(route) {
     routerObserver.next('changeRoute',route);
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import Store from "./store/index.js";
+import Store from "./store.js";
 import createReducers from "./reducers.js";
 
 export default new Store(createReducers());
